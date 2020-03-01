4dff7ab76c995bc9db2a58b867af86d825007b38
shrouknadytaha <36441658+shrouknadytaha@users.noreply.github.com>
2020-03-01 15:04:43 +0200

    After calling class AreaSphere_20170128
    class class to calculate Area of sphere

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	private static ISubscriber subscribers [] = {
 			new SimpleSubscriber(),
 			new ReallySimpleSubscriber(),
+		
 			new AreaSphere(),
 	};
 	public static void main(String[] args) {
